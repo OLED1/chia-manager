20ad863c904b84aeb38786800094514ddad95171
Oled <oliver@edtmair.at>
2021-08-31 16:42:15 +0200

    Further work on updater.

--- backend/core/System_Update/System_Update_Api.php
+++ backend/core/System_Update/System_Update_Api.php
@@ -2,11 +2,12 @@
   namespace ChiaMgmt\System_Update;
   use ChiaMgmt\DB\DB_Api;
   use ChiaMgmt\WebSocket\WebSocket_Api;
+  use ChiaMgmt\System\System_Api;
 
   class System_Update_Api{
     private $db_api, $ini, $ciphering, $iv_length, $options, $encryption_iv, $websocket_api;
+
     public function __construct(){
-      //Variables for pw encrypting and decrypting
       $this->ciphering = "AES-128-CTR";
       $this->iv_length = openssl_cipher_iv_length($this->ciphering);
       $this->options = 0;
@@ -18,8 +19,6 @@
     }
 
     public function processUpdate(array $data, array $loginData = NULL, $server = NULL){
-      print_r($data);
-      print_r($loginData);
       $this->server = $server;
       $now = new \DateTime();
       $now = $now->format("Y-m-d H:i:s");
@@ -28,6 +27,7 @@
       $this->createBackupdirs("Creating backup directories", $now);
       if(!$this->preverror) $this->backupSystemData("Backing up system data", $now);
       if(!$this->preverror) $this->backupSystemDatabase("Backing up databasedata", $now);
+      if(!$this->preverror) $this->downloadUpdateData("Downloading and installing update");
 
       if($this->preverror){
         //TODO Implement correct status code
@@ -38,7 +38,6 @@
     }
 
     private function createBackupdirs(string $message, string $timestamp){
-      set_error_handler("exception_error_handler");
       $this->sendStatus(0, 1, 2, $message);
       $backupdir = "../../../..{$this->ini["backup_root"]}";
 
@@ -104,26 +103,6 @@
       }
     }
 
-    private function full_copy($source, $dest){
-      if(!file_exists($dest)) mkdir($dest, 0755);
-      foreach(
-       $iterator = new \RecursiveIteratorIterator(
-                    new \RecursiveDirectoryIterator($source, \RecursiveDirectoryIterator::SKIP_DOTS),
-                        \RecursiveIteratorIterator::SELF_FIRST) as $item
-      ){
-        if(str_contains($iterator->getSubPathname(), "backup" )){
-          continue;
-        }else{
-          if($item->isDir()){
-            mkdir($dest . DIRECTORY_SEPARATOR . $iterator->getSubPathname());
-          }else{
-            echo "Copying {$item} -> " . $dest . DIRECTORY_SEPARATOR . $iterator->getSubPathname() ."\n";
-            copy($item, $dest . DIRECTORY_SEPARATOR . $iterator->getSubPathname());
-          }
-        }
-      }
-    }
-
     private function zipBackup($source, $dest, $timestamp){
       $zip = new \ZipArchive();
       $zip->open("{$dest}/backup-{$timestamp}.zip", \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
@@ -144,8 +123,71 @@
       $zip->close();
     }
 
-    private function downloadUpdateData(){
-      
+    private function downloadUpdateData($message){
+      $this->sendStatus(0, 4, 2, $message);
+      $updatepackagepath = "https://files.chiamgmt.edtmair.at/server/";
+      $version_file_json = file_get_contents("{$updatepackagepath}/versions.json");
+      $version_file_data = json_decode($version_file_json, true);
+
+      $system_api = new System_Api();
+      $updatechannel = $system_api->getSpecificSystemSetting("updatechannel");
+      if(array_key_exists("updatechannel", $updatechannel["data"])){
+        $updatechannel = $updatechannel["data"]["updatechannel"]["branch"]["value"];
+      }else{ $updatechannel = "main"; }
+
+      if(!is_null($version_file_data) && array_key_exists($updatechannel, $version_file_data)){
+        print_r($version_file_data);
+
+        $updateurl = $version_file_data[$updatechannel][0]["link"];
+        $tmpdir = "/tmp";
+        if(is_dir($tmpdir)){
+          $packagepath = "{$updatepackagepath}{$updateurl}";
+          $tmpfiledir = "{$tmpdir}/chiamgmt_update.zip";
+
+          file_put_contents($tmpfiledir,file_get_contents($packagepath));
+
+          $zip = new \ZipArchive;
+          $res = $zip->open($tmpfiledir);
+          if ($res === TRUE) {
+            $zip->extractTo($tmpdir);
+            $zip->close();
+
+            $this->full_copy("{$tmpdir}/chia-web-gui-{$updatechannel}/", "../../../..{$this->ini["system_root"]}/");
+            $this->sendStatus(0, 4, 0, $message);
+          }else{
+            $this->preverror = true;
+            $this->sendStatus(0, 4, 1, "{$message}. Could not open {$tmpfiledir}.");
+            return;
+          }
+        }else{
+          $this->preverror = true;
+          $this->sendStatus(0, 4, 1, "{$message}. Temporary dir /tmp not found.");
+          return;
+        }
+      }else{
+        $this->preverror = true;
+        $this->sendStatus(0, 4, 1, $message);
+      }
+    }
+
+    private function full_copy($source, $dest){
+      if(!file_exists($dest)) mkdir($dest, 0755);
+      foreach(
+       $iterator = new \RecursiveIteratorIterator(
+                    new \RecursiveDirectoryIterator($source, \RecursiveDirectoryIterator::SKIP_DOTS),
+                        \RecursiveIteratorIterator::SELF_FIRST) as $item
+      ){
+        if(str_contains($iterator->getSubPathname(), "backup" )){
+          continue;
+        }else{
+          if($item->isDir()){
+            mkdir($dest . DIRECTORY_SEPARATOR . $iterator->getSubPathname());
+          }else{
+            echo "Copying {$item} -> " . $dest . DIRECTORY_SEPARATOR . $iterator->getSubPathname() ."\n";
+            copy($item, $dest . DIRECTORY_SEPARATOR . $iterator->getSubPathname());
+          }
+        }
+      }
     }
 
     //processing-status: 0 Success, 1 Failed, 2 Processing
