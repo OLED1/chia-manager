5ad60bd4f2d8a905ec600af2af02414e4c5b4631
OLED01 <oliver@edtmair.at>
2022-02-16 16:31:08 +0100

    Cron will not restart websocket during update.

--- backend/core/CronBackendService/CronBackendService.php
+++ backend/core/CronBackendService/CronBackendService.php
@@ -1,6 +1,7 @@
 <?php
   namespace ChiaMgmt\CronBackendService;
 
+  use ChiaMgmt\System_Update\System_Update_Api;
   use ChiaMgmt\WebSocket\WebSocket_Api;
   use ChiaMgmt\Logging\Logging_Api;
 
@@ -29,6 +30,7 @@
      */
     public function __construct(){
       $this->websocket_api = new WebSocket_Api();
+      $this->system_update_api = new System_Update_Api();
       $this->logging_api = new Logging_Api($this);
     }
 
@@ -36,22 +38,27 @@
      * Queries information from all nodes by calling the websocket's command "queryCronData"
      */
     public function queryData(){
-      echo "{$this->getDate()}: {$this->logging_api->getErrormessage("001")["message"]}\n";
-      $wssstatus = $this->websocket_api->testConnection();
-      echo "{$this->getDate()}: {$wssstatus["message"]}\n";
-
-      if($wssstatus["status"] == "016001002"){
-        echo "{$this->getDate()}: {$this->logging_api->getErrormessage("002")["message"]}\n";
-        $wssstatus = $this->websocket_api->startWSS();
+      $system_update_state = $this->system_update_api->checkUpdateRoutine();
+      if($system_update_state["data"]["maintenance_mode"] == 0 && $system_update_state["data"]["process_update"] == 0){
+        echo "{$this->getDate()}: {$this->logging_api->getErrormessage("001")["message"]}\n";
+        $wssstatus = $this->websocket_api->testConnection();
         echo "{$this->getDate()}: {$wssstatus["message"]}\n";
-      };
-
-      if($wssstatus["status"] == 0){
-        echo "{$this->getDate()}: {$this->logging_api->getErrormessage("003")["message"]}\n";
-        $cronExec = $this->websocket_api->sendToWSS("queryCronData")["cronJobExecution"];
-        $loglevel = ($cronExec["status"] == 0 ? 0 : 2);
-        $this->logging_api->logtofile($loglevel, 0, $cronExec["message"]);
-        echo "{$this->getDate()}: {$this->logging_api->getErrormessage("004")["message"]}\n";
+  
+        if($wssstatus["status"] == "016001002"){
+          echo "{$this->getDate()}: {$this->logging_api->getErrormessage("002")["message"]}\n";
+          $wssstatus = $this->websocket_api->startWSS();
+          echo "{$this->getDate()}: {$wssstatus["message"]}\n";
+        };
+  
+        if($wssstatus["status"] == 0){
+          echo "{$this->getDate()}: {$this->logging_api->getErrormessage("003")["message"]}\n";
+          $cronExec = $this->websocket_api->sendToWSS("queryCronData")["cronJobExecution"];
+          $loglevel = ($cronExec["status"] == 0 ? 0 : 2);
+          $this->logging_api->logtofile($loglevel, 0, $cronExec["message"]);
+          echo "{$this->getDate()}: {$this->logging_api->getErrormessage("004")["message"]}\n";
+        }
+      }else{
+        echo "{$this->getDate()}: {$this->logging_api->getErrormessage("005")["message"]}\n";
       }
     }
 
