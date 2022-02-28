e574e4db7e60919cc8c48053ef1521456b6c8f23
OLED01 <oliver@edtmair.at>
2022-02-28 14:08:48 +0100

    Icon fix.

--- frontend/sites/system/index.php
+++ frontend/sites/system/index.php
@@ -342,7 +342,7 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title"><span class="fas fa-paper-plane"></span>&nbsp;Release Notes (Version:&nbsp;<span id="release-version"></span>)</h5>
+        <h5 class="modal-title"><span class="fa-solid fa-note-sticky"></span>&nbsp;Release Notes (Version:&nbsp;<span id="release-version"></span>)</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
